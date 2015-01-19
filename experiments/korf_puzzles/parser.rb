63961948675ade144a260d7a0512e394d937b871
Yuri Blokhin <ultrablox@gmail.com>
2015-01-19 17:07:44 +0300

    Added corf puzzle instances.

---
+++ experiments/korf_puzzles/parser.rb
@@ -0,0 +1,12 @@
+
+main_file = File.new("all_data.txt", "r")
+
+
+i = 1
+while (inst = main_file.gets)
+    file = File.open("korf_" + i.to_s + ".txt", "w")
+    file.write("4 4\n")
+    file.write(inst)
+    file.close
+    i += 1
+end
