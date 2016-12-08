0c4496263617612f4dc1d288ff4de5d880c88111
Levon <diplaySuper@gmail.com>
2016-12-08 18:53:42 +0300

    more test cases

--- src/tests/test_edfd_cover.cpp
+++ src/tests/test_edfd_cover.cpp
@@ -47,6 +47,7 @@ void test_equal_graph()
 }
 
 //! source graph contains two subgraphs which are equal to one SDP graph
+//! this subgraphs are not connected
 //! available SDP set contains one graph
 //! expected result: two transitions - cover each source subgraph with only SDP
 void test_graph_with_two_subgraphs()
@@ -68,8 +69,8 @@ void test_graph_with_two_subgraphs()
 	);
 
 	edfd_graph sdp(
-	{ one, two, three },
-	{ { one, two }, { one, three }, { two, three } }
+		{ one, two, three },
+		{ { one, two }, { one, three }, { two, three } }
 	);
 	vector<edfd_graph> sdps{ sdp };
 	transition_system<edfd_cover> ts(sdps);
@@ -92,6 +93,106 @@ void test_graph_with_two_subgraphs()
 	assert_test(result, "test_graph_with_two_subgraphs");
 }
 
+//! source graph contains two subgraphs which are equal to one SDP graph
+//! this subgraphs are connected with each other
+//! available SDP set contains one graph
+//! expected result: two transitions - cover each source subgraph with only SDP
+void test_graph_with_two_connected_subgraphs()
+{
+	edfd_element one  { 1, "one", edfd_element::type_t::entity };
+	edfd_element two  { 2, "two", edfd_element::type_t::entity };
+	edfd_element three{ 3, "three", edfd_element::type_t::entity };
+
+	edfd_element four{ 4, "four", edfd_element::type_t::entity };
+	edfd_element five{ 5, "five", edfd_element::type_t::entity };
+	edfd_element six { 6, "six", edfd_element::type_t::entity };
+
+	edfd_graph source_graph(
+		{ one, two, three, four, five, six },
+		{
+			{ one, two }, { one, three }, { two, three },
+			{ four, five }, { four, six }, { five, six },
+			{ one, five }, { six, three } //connection between two parts
+		}
+	);
+
+	edfd_graph sdp(
+		{ one, two, three },
+		{ { one, two }, { one, three }, { two, three } }
+	);
+	vector<edfd_graph> sdps{ sdp };
+	transition_system<edfd_cover> ts(sdps);
+
+	edfd_cover_state state;
+	state.src_graph = source_graph;
+
+	vector<typename transition_system<edfd_cover>::transition_t> available_transitions;
+	transition_system<edfd_cover>::transition_t tr;
+
+	tr.new_sdp_instance = 0;
+	tr.cover_difference = { 1, 2, 3 }; //first three vertices are covered with 0'th sdp instance
+	available_transitions.push_back(tr);
+
+	tr.new_sdp_instance = 0;
+	tr.cover_difference = { 4, 5, 6 }; //second three vertices are covered with 0'th sdp instance
+	available_transitions.push_back(tr);
+
+	bool result = test_available_transitions(ts, state, available_transitions);
+	assert_test(result, "test_graph_with_two_connected_subgraphs");
+}
+
+//! source graph contains three subgraphs each of them is equal to one SDP graph
+//! two subgraphs is equal to each other and are part of third graph
+//! available SDP set contains two graphs
+//! one graph is part of another
+//! expected result: three transitions - cover each source subgraph with corresponding SDP
+void test_graph_with_included_subgraphs()
+{
+	edfd_element one  { 1, "one", edfd_element::type_t::entity };
+	edfd_element two  { 2, "two", edfd_element::type_t::entity };
+	edfd_element three{ 3, "three", edfd_element::type_t::entity };
+	edfd_element four { 4, "four", edfd_element::type_t::entity };
+
+	edfd_graph source_graph(
+		{ one, two, three, four},
+		{
+			{ one, two }, { one, three }, { two, three },
+			{ four, one }, { four, two }
+		}
+	);
+
+	edfd_graph sdp1(
+		{ one, two, three },
+		{ { one, two }, { one, three }, { two, three } }
+	);
+
+	edfd_graph sdp2{ source_graph };
+
+	vector<edfd_graph> sdps{ sdp1, sdp2 };
+	transition_system<edfd_cover> ts(sdps);
+
+	edfd_cover_state state;
+	state.src_graph = source_graph;
+
+	vector<typename transition_system<edfd_cover>::transition_t> available_transitions;
+	transition_system<edfd_cover>::transition_t tr;
+
+	tr.new_sdp_instance = 0;
+	tr.cover_difference = { 1, 2, 3 }; //first three vertices are covered with 0'th sdp instance
+	available_transitions.push_back(tr);
+
+	tr.new_sdp_instance = 0;
+	tr.cover_difference = { 4, 1, 2 }; //first three vertices are covered with 0'th sdp instance
+	available_transitions.push_back(tr);
+
+	tr.new_sdp_instance = 1;
+	tr.cover_difference = { 1, 2, 3, 4 }; //second three vertices are covered with 1'st sdp instance
+	available_transitions.push_back(tr);
+
+	bool result = test_available_transitions(ts, state, available_transitions);
+	assert_test(result, "test_graph_with_included_subgraphs");
+}
+
 void test_edfd_cover()
 {
 	cout << "Testing edfd cover" << endl;
@@ -99,4 +200,6 @@ void test_edfd_cover()
 	test_empty();
 	test_equal_graph();
 	test_graph_with_two_subgraphs();
+	test_graph_with_two_connected_subgraphs();
+	test_graph_with_included_subgraphs();
 }
